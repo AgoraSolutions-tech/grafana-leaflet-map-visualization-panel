592c0909702132a754c8d9e8b1bce42b5af12e33
nmateusiak <nmat@thaumatec.com>
2023-09-14 09:37:20 +0200

    Adding the react-leaflet-draw library, creating logic for onCreated, onEdited, and onDeleted events, and integrating it with existing functionality

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { PanelProps } from '@grafana/data';
-import { TileLayer, MapContainer, Tooltip, Polygon, Marker, Polyline, Popup } from 'react-leaflet';
+import { PanelProps, urlUtil } from '@grafana/data';
+import { TileLayer, MapContainer, Tooltip, Polygon, Marker, Polyline, Popup, FeatureGroup } from 'react-leaflet';
+import { EditControl } from 'react-leaflet-draw';
 import L, { LatLngExpression } from 'leaflet';
-import { Area, Boat, MapOptions } from 'types';
+import { Area, Boat, MapOptions, Vertex } from 'types';
 import { cx, css } from '@emotion/css';
 import { useStyles2 } from '@grafana/ui';
+import { MapContainerDescendant } from './MapContainerDescendant';
+import { uniqueId } from 'helpers';
+import show from '../img/show.png';
+import hide from '../img/hide.png';
 
 import 'leaflet/dist/leaflet.css';
-import { MapContainerDescendant } from './MapContainerDescendant';
+import 'leaflet-draw/dist/leaflet.draw.css';
 
 interface Props extends PanelProps<MapOptions> {}
 
@@ -17,25 +22,45 @@ const getStyles = () => {
       font-family: Open Sans;
     `,
     styledTooltip: css`
-      background: none;
-      font-size: 15px;
+      background: none !important;
+      border: none !important;
+      box-shadow: none !important;
+      font-weight: 800;
+      font-size: 18px;
+      text-shadow: 1px 1px 2px white;
+    `,
+    styledButton: css`
+      background-color: white;
+      color: black;
+      font-size: 14px;
+      border-radius: 4px;
+      border: 2px solid rgba(0, 0, 0, 0.2);
+      background-clip: padding-box;
+      cursor: pointer !important;
+    `,
+    styledImg: css`
+      width: 20px;
+      margin: 5px;
     `,
   };
 };
-
 export const MapComponent: React.FC<Props> = ({ options, width, height, data, onOptionsChange }) => {
   const styles = useStyles2(getStyles);
   const [historyVisibility, setHistoryVisibility] = useState(false);
+  const [editContolKey, setEditControlKey] = useState(uniqueId());
 
   const mapCenter: LatLngExpression = [options.lat, options.lng];
   const areas = options.areas?.areas || [];
   const zoomValue = options.zoom;
-
+  const isTooltipSticky = options.areas.isTooltipSticky;
   const boatIcon = L.icon({
     iconUrl: require('../img/boat-pin.svg'),
-    iconSize: [40, 40],
+    iconSize: [50, 50],
+    iconAnchor: [25, 45],
   });
-  
+
+  const params = urlUtil.getUrlSearchParams();
+  const isEditMode = !!params.editPanel;
 
   const query = options.boatQuery;
   const boatsData = data.series.find((s) => s.refId === query)?.fields || [];
@@ -71,6 +96,114 @@ export const MapComponent: React.FC<Props> = ({ options, width, height, data, on
     onOptionsChange({ ...options, lat: position.lat, lng: position.lng, zoom: newValue });
   };
 
+  const onCreated = (e: any) => {
+    const { layerType, layer } = e;
+    if (layerType === 'polygon') {
+      const { _latlngs } = layer;
+      if (!_latlngs[0]) {
+        return;
+      }
+
+      const newVerticles = _latlngs[0];
+      onOptionsChange({
+        ...options,
+        areas: {
+          ...options.areas,
+          areas: [
+            ...areas,
+            {
+              id: uniqueId(),
+              name: `Area ${areas.length + 1}`,
+              color: 'red',
+              verticles: newVerticles.map((vertex: Vertex) => ({
+                id: uniqueId(),
+                lat: vertex.lat,
+                lng: vertex.lng,
+              })),
+            },
+          ],
+        },
+      });
+      setEditControlKey(uniqueId());
+    }
+  };
+
+  const onEdited = (e: any) => {
+    const { layers } = e;
+    const layerIds = Object.keys(layers._layers);
+
+    if (!layerIds[0]) {
+      return;
+    }
+
+    const polygonLayerId = layerIds[0];
+    const polygon = layers._layers[polygonLayerId];
+
+    const polygonId = polygon.options.id;
+    const newVerticles = polygon._latlngs[0];
+
+    const area = areas.find((area) => area.id === polygonId);
+
+    if (!area) {
+      return;
+    }
+
+    const newAreas = areas.map((area) => {
+      if (area.id !== polygonId) {
+        return area;
+      }
+
+      return {
+        ...area,
+        verticles: newVerticles.map((vertex: Vertex) => ({
+          id: uniqueId(),
+          lat: vertex.lat,
+          lng: vertex.lng,
+        })),
+      };
+    });
+
+    onOptionsChange({
+      ...options,
+      areas: {
+        ...options.areas,
+        areas: newAreas,
+      },
+    });
+    setEditControlKey(uniqueId());
+  };
+
+  const onDeleted = (e: any) => {
+    const { layers } = e;
+    const layerIds = Object.keys(layers._layers).map((id) => Number(id));
+
+    if (!layerIds[0]) {
+      return;
+    }
+
+    const removedLayers = layers._layers;
+    let removedPoligonsIds: any = [];
+    for (let i = 0; i < layerIds.length; i++) {
+      const id: number = removedLayers[layerIds[i]].options.id;
+      removedPoligonsIds.push(id);
+    }
+    const newAreas = areas.filter((area) => {
+      if (removedPoligonsIds.includes(area.id)) {
+        return false;
+      }
+      return true;
+    });
+
+    onOptionsChange({
+      ...options,
+      areas: {
+        ...options.areas,
+        areas: newAreas,
+      },
+    });
+    setEditControlKey(uniqueId());
+  };
+
   return (
     <MapContainer
       center={mapCenter}
@@ -89,24 +222,50 @@ export const MapComponent: React.FC<Props> = ({ options, width, height, data, on
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <MapContainerDescendant onMapEventTrigger={handleMapEventTrigger} />
-      {areas.map((area: Area, index: number) => {
-        return (
-          <div key={index}>
-            <Polygon positions={area.verticles} pathOptions={{ color: area.color }}>
-              {options.zoom >= 14 && (
-                <Tooltip
-                  sticky={!options.areas.isTooltipSticky}
-                  permanent={options.areas.isTooltipSticky}
-                  direction="center"
-                  className={styles.styledTooltip}
-                >
-                  {area.name}
-                </Tooltip>
-              )}
-            </Polygon>
-          </div>
-        );
-      })}
+      <FeatureGroup>
+        {isEditMode && (
+          <EditControl
+            key={editContolKey}
+            position="topright"
+            onCreated={onCreated}
+            onEdited={onEdited}
+            onDeleted={onDeleted}
+            draw={{
+              polyline: false,
+              rectangle: false,
+              circle: false,
+              marker: false,
+              circlemarker: false,
+            }}
+          />
+        )}
+        {areas.map((area: Area, index: number) => {
+          return (
+            <div key={area.id}>
+              <Polygon
+                positions={area.verticles}
+                pathOptions={{
+                  color: area.color,
+                  // @ts-ignore
+                  id: area.id,
+                }}
+              >
+                {options.zoom >= 15 && (
+                  <Tooltip
+                    key={isTooltipSticky ? 1 : 0}
+                    sticky={!isTooltipSticky}
+                    permanent={isTooltipSticky}
+                    direction="center"
+                    className={styles.styledTooltip}
+                  >
+                    {area.name}
+                  </Tooltip>
+                )}
+              </Polygon>
+            </div>
+          );
+        })}
+      </FeatureGroup>
       {boats &&
         boats.map((boat) => {
           const position = [
@@ -123,6 +282,7 @@ export const MapComponent: React.FC<Props> = ({ options, width, height, data, on
                   {boat.positions[boat.positions.length - 1].lng}
                 </Popup>
               </Marker>
+
               {historyVisibility &&
                 linePoints.map((point, index) => {
                   if (index + 1 >= linePoints.length) {
@@ -141,9 +301,13 @@ export const MapComponent: React.FC<Props> = ({ options, width, height, data, on
             </>
           );
         })}
-      <div className="leaflet-bottom leaflet-right">
-        <button className="leaflet-control" onClick={() => setHistoryVisibility(!historyVisibility)}>
-          click
+      <div className="leaflet-bottom leaflet-left">
+        <button
+          className={'leaflet-control' + ' ' + styles.styledButton}
+          onClick={() => setHistoryVisibility(!historyVisibility)}
+        >
+          <img src={historyVisibility ? show : hide} className={styles.styledImg} />
+          {'trail'.toUpperCase()}
         </button>
       </div>
     </MapContainer>
